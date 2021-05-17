b24f312d9ff2657de7352cd9963d560e4bad7e10
DamienButterworth <damienbutterworth@hotmail.co.uk>
2021-05-17 12:34:49 +0100

    MMCA-2842: changed assertion into yield

--- test/uk/gov/hmrc/customs/datastore/repositories/EmailRepositorySpec.scala
+++ test/uk/gov/hmrc/customs/datastore/repositories/EmailRepositorySpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.customs.datastore.repositories
 
 import org.joda.time.DateTime
@@ -32,12 +48,13 @@ class EmailRepositorySpec extends SpecBase {
     await(for {
       _ <- repository.set(eori, notificationEmail)
       currentNotification <- repository.get(eori)
-      _ = currentNotification mustBe Some(notificationEmail)
       _ <- repository.update(undeliverableInformation)
       newNotification <- repository.get(eori)
-      _ = newNotification mustBe Some(notificationEmail.copy(undeliverable = Some(undeliverableInformation)))
       _ <- dropData()
-    } yield {})
+    } yield {
+      currentNotification mustBe Some(notificationEmail)
+      newNotification mustBe Some(notificationEmail.copy(undeliverable = Some(undeliverableInformation)))
+    })
   }
 
   "return 'false' if no update performed" in {
@@ -51,9 +68,10 @@ class EmailRepositorySpec extends SpecBase {
       result <- repository.update(undeliverableInformation)
       _ = result mustBe false
       record <- repository.get(eori)
-      _ = record mustBe None
       _ <- dropData()
-    } yield {})
+    } yield {
+      record mustBe None
+    })
   }
 
   "remove the undeliverable object when setting a new email address" in {
@@ -65,11 +83,12 @@ class EmailRepositorySpec extends SpecBase {
       _ <- repository.set(eori, notificationEmail)
       _ <- repository.update(undeliverableInformation)
       firstResult <- repository.get(eori)
-      _ = firstResult mustBe Some(notificationEmail.copy(undeliverable = Some(undeliverableInformation)))
       _ <- repository.set(eori, notificationEmail)
       secondResult <- repository.get(eori)
-      _ = secondResult mustBe Some(notificationEmail)
       _ <- dropData()
-    } yield {})
+    } yield {
+      firstResult mustBe Some(notificationEmail.copy(undeliverable = Some(undeliverableInformation)))
+      secondResult mustBe Some(notificationEmail)
+    })
   }
 }
