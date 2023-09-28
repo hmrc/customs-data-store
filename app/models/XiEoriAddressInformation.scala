d1c10e060bd361445aaad248b010c176c310f791
harsha <29434052+harshare@users.noreply.github.com>
2023-09-28 12:32:20 +0100

    MMCA-4278 Set default value to optional address fields

--- app/models/XiEoriAddressInformation.scala
+++ app/models/XiEoriAddressInformation.scala
@@ -19,10 +19,10 @@ package models
 import play.api.libs.json.{Json, OFormat}
 
 case class XiEoriAddressInformation(pbeAddressLine1: String,
-                                    pbeAddressLine2: Option[String],
-                                    pbeAddressLine3: Option[String],
-                                    pbeAddressLine4: Option[String],
-                                    pbePostCode: Option[String])
+                                    pbeAddressLine2: Option[String] = None,
+                                    pbeAddressLine3: Option[String] = None,
+                                    pbeAddressLine4: Option[String] = None,
+                                    pbePostCode: Option[String] = None)
 
 object XiEoriAddressInformation {
   implicit val format: OFormat[XiEoriAddressInformation] = Json.format[XiEoriAddressInformation]
