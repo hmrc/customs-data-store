2f0d411f6b8dbdda30331f8b18de124575114b1a
vineet <vineet.tyagi@digital.hmrc.gov.uk>
2024-07-18 13:31:39 +0100

    [MMCA-4789] Add existing code taken from financials api

---
+++ test/controllers/SubscriptionControllerSpec.scala
@@ -0,0 +1,137 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import config.MetaConfig.Platform.{ENROLMENT_IDENTIFIER, ENROLMENT_KEY}
+import models.{EORI, EmailAddress}
+import org.mockito.ArgumentMatchers.eq as is
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.mvc.AnyContentAsEmpty
+import play.api.test.FakeRequest
+import play.api.test.Helpers.*
+import play.api.{Application, inject}
+import services.SubscriptionService
+import uk.gov.hmrc.auth.core.{Enrolment, EnrolmentIdentifier, Enrolments}
+import uk.gov.hmrc.http.NotFoundException
+import utils.SpecBase
+import utils.TestData.EORI_VALUE
+
+import scala.concurrent.Future
+
+class SubscriptionControllerSpec extends SpecBase {
+
+  "SubscriptionController.get" should {
+    "return 200 status code" in new Setup {
+      val subscriptionResponse: EmailVerifiedResponse = EmailVerifiedResponse(None)
+
+      when(mockSubscriptionService.getVerifiedEmail(is(traderEORI)))
+        .thenReturn(Future.successful(subscriptionResponse))
+
+      running(app) {
+        val result = route(app, request).value
+        status(result) mustBe OK
+      }
+    }
+
+    "return 200 status code for unVerified email" in new Setup {
+      val subscriptionResponse: EmailUnverifiedResponse = EmailUnverifiedResponse(None)
+
+      when(mockSubscriptionService.getUnverifiedEmail(is(traderEORI)))
+        .thenReturn(Future.successful(subscriptionResponse))
+
+      running(app) {
+        val result = route(app, unVerifiedEmailRequest).value
+        status(result) mustBe OK
+      }
+    }
+
+    "return 200 status code for getEmail" in new Setup {
+      val subscriptionResponse: EmailVerifiedResponse = EmailVerifiedResponse(Some(EmailAddress("test@email.com")))
+
+      when(mockSubscriptionService.getEmailAddress(is(traderEORI)))
+        .thenReturn(Future.successful(subscriptionResponse))
+
+      running(app) {
+        val result = route(app, getEmailAddressrequest).value
+        status(result) mustBe OK
+      }
+    }
+
+    "return 503 for any error" in new Setup {
+      when(mockSubscriptionService.getVerifiedEmail(is(traderEORI)))
+        .thenReturn(Future.failed(new NotFoundException("ShouldNotReturnThis")))
+
+      running(app) {
+        val result = route(app, request).value
+        status(result) mustBe SERVICE_UNAVAILABLE
+      }
+    }
+
+    "return 503 for any error in unverified email" in new Setup {
+      when(mockSubscriptionService.getUnverifiedEmail(is(traderEORI)))
+        .thenReturn(Future.failed(new NotFoundException("ShouldNotReturnThis")))
+
+      running(app) {
+        val result = route(app, unVerifiedEmailRequest).value
+        status(result) mustBe SERVICE_UNAVAILABLE
+      }
+    }
+
+    "return 503 for any error in getEmail" in new Setup {
+      when(mockSubscriptionService.getEmailAddress(is(traderEORI)))
+        .thenReturn(Future.failed(new NotFoundException("ShouldNotReturnThis")))
+
+      running(app) {
+        val result = route(app, getEmailAddressrequest).value
+        status(result) mustBe SERVICE_UNAVAILABLE
+      }
+    }
+  }
+
+  trait Setup {
+
+    val traderEORI: EORI = EORI(EORI_VALUE)
+    val enrolments: Enrolments =
+      Enrolments(
+        Set(Enrolment(ENROLMENT_KEY,
+          Seq(EnrolmentIdentifier(ENROLMENT_IDENTIFIER, traderEORI.value)),
+          "activated")))
+
+    val request: FakeRequest[AnyContentAsEmpty.type] =
+      FakeRequest("GET", controllers.routes.SubscriptionController.getVerifiedEmail().url)
+
+    val getEmailAddressrequest: FakeRequest[AnyContentAsEmpty.type] =
+      FakeRequest("GET", controllers.routes.SubscriptionController.getEmail().url)
+
+    val unVerifiedEmailRequest: FakeRequest[AnyContentAsEmpty.type] =
+      FakeRequest("GET", controllers.routes.SubscriptionController.getUnverifiedEmail().url)
+
+    val mockAuthConnector: CustomAuthConnector = mock[CustomAuthConnector]
+    val mockSubscriptionService: SubscriptionService = mock[SubscriptionService]
+
+    when(mockAuthConnector.authorise[Enrolments](any, any)(any, any)).thenReturn(Future.successful(enrolments))
+
+    val app: Application = GuiceApplicationBuilder().overrides(
+      inject.bind[CustomAuthConnector].toInstance(mockAuthConnector),
+      inject.bind[SubscriptionService].toInstance(mockSubscriptionService)
+    ).configure(
+      "microservice.metrics.enabled" -> false,
+      "metrics.enabled" -> false,
+      "auditing.enabled" -> false
+    ).build()
+  }
+}
---
+++ test/services/SubscriptionServiceSpec.scala
@@ -0,0 +1,179 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package services
+
+import connectors.Sub09Connector
+import models.{EORI, EmailAddress}
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.test.Helpers.running
+import play.api.{Application, inject}
+import uk.gov.hmrc.http.HeaderCarrier
+import utils.SpecBase
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class SubscriptionServiceSpec extends SpecBase {
+
+  "SubscriptionService" when {
+
+    "get verifiedEmail" should {
+      "return verified email when there is contactInformation with timestamp" in new Setup {
+        when(mockSub09Connector.getSubscriptions(EORI("Trader EORI"))).thenReturn(
+          Future.successful(subscriptionResponseWithTimestamp))
+
+        running(app) {
+          val result = service.getVerifiedEmail(EORI("Trader EORI"))
+          result.map {
+            ev => ev mustBe EmailVerifiedResponse(Option(EmailAddress(emailAddress)))
+          }
+        }
+      }
+
+      "return None when there is no contactInformation" in new Setup {
+        when(mockSub09Connector.getSubscriptions(EORI("Trader EORI"))).thenReturn(
+          Future.successful(subscriptionResponse))
+
+        running(app) {
+          val result = service.getVerifiedEmail(EORI("Trader EORI"))
+          result.map {
+            ev => ev mustBe EmailVerifiedResponse(None)
+          }
+        }
+      }
+
+      "return None when there is no timestamp in contactInformation" in new Setup {
+        when(mockSub09Connector.getSubscriptions(EORI("Trader EORI"))).thenReturn(
+          Future.successful(subscriptionResponseWithContactInfo))
+
+        running(app) {
+          val result = service.getVerifiedEmail(EORI("Trader EORI"))
+          result.map {
+            ev => ev mustBe EmailVerifiedResponse(None)
+          }
+        }
+      }
+    }
+
+    "getEmailAddress" should {
+      "return correct output when there is no contactInformation" in new Setup {
+        when(mockSub09Connector.getSubscriptions(any)).thenReturn(Future.successful(subscriptionResponse))
+
+        running(app) {
+          val result: Future[EmailVerifiedResponse] = service.getEmailAddress(EORI("Trader EORI"))
+
+          result.map {
+            ev => ev mustBe EmailVerifiedResponse(None)
+          }
+        }
+      }
+
+      "return correct output when contactInformation is available" in new Setup {
+        when(mockSub09Connector.getSubscriptions(any)).thenReturn(Future.successful(subscriptionResponseWithContactInfo))
+
+        running(app) {
+          val result: Future[EmailVerifiedResponse] = service.getEmailAddress(EORI("Trader EORI"))
+
+          result.map {
+            ev => ev mustBe EmailVerifiedResponse(Option(EmailAddress(emailAddress)))
+          }
+        }
+      }
+    }
+
+    "getUnverifiedEmail" should {
+      "return correct output when there is no contactInformation" in new Setup {
+        when(mockSub09Connector.getSubscriptions(any)).thenReturn(Future.successful(subscriptionResponse))
+
+        running(app) {
+          val result: Future[EmailUnverifiedResponse] = service.getUnverifiedEmail(EORI("Trader EORI"))
+
+          result.map {
+            eunv => eunv mustBe EmailUnverifiedResponse(None)
+          }
+        }
+      }
+
+      "return correct output when contactInformation is available" in new Setup {
+        when(mockSub09Connector.getSubscriptions(any)).thenReturn(Future.successful(
+          subscriptionResponseWithContactInfo))
+
+        running(app) {
+          val result: Future[EmailUnverifiedResponse] = service.getUnverifiedEmail(EORI("Trader EORI"))
+
+          result.map {
+            eunv => eunv mustBe EmailUnverifiedResponse(Option(EmailAddress(emailAddress)))
+          }
+        }
+      }
+    }
+  }
+
+  trait Setup {
+    implicit val hc: HeaderCarrier = HeaderCarrier()
+    implicit val executionContext: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
+
+    val eori: EORI = EORI("testEORI")
+    val mockSub09Connector: Sub09Connector = mock[Sub09Connector]
+
+    val app: Application = GuiceApplicationBuilder().overrides(
+      inject.bind[Sub09Connector].toInstance(mockSub09Connector)
+    ).configure(
+      "microservice.metrics.enabled" -> false,
+      "metrics.enabled" -> false,
+      "auditing.enabled" -> false
+    ).build()
+
+    val service: SubscriptionService = app.injector.instanceOf[SubscriptionService]
+
+    val responseCommon: ResponseCommon = ResponseCommon("OK", None, "2020-10-05T09:30:47Z", None)
+    val cdsEstablishmentAddress: CdsEstablishmentAddress = CdsEstablishmentAddress(
+      "1 street", "Southampton", Some("SO1 1AA"), "GB")
+
+    val vatIds: VatId = VatId(Some("abc"), Some("123"))
+    val euVatIds: EUVATNumber = EUVATNumber(Some("def"), Some("456"))
+
+    val xiEoriAddress: PbeAddress = PbeAddress("1 Test street", Some("city A"), Some("county"), None, Some("AA1 1AA"))
+    val xiEoriSubscription: XiSubscription = XiSubscription("XI1234567", Some(xiEoriAddress), Some("1"),
+      Some("12345"), Some(Array(euVatIds)), "1", Some("abc"))
+
+    val responseDetail: ResponseDetail = ResponseDetail(Some(EORI("someEori")), None, None, "CDSFullName",
+      cdsEstablishmentAddress, Some("0"), None, None, Some(Array(vatIds)),
+      None, None, None, None, None, None, ETMP_Master_Indicator = true, Some(xiEoriSubscription))
+
+    val emailAddress = "test@gmil.com"
+
+    val contactInfo: ContactInformation = ContactInformation(None, None, None, None, None, None, None, None,
+      emailAddress = Some(EmailAddress(emailAddress)), None)
+
+    val contactInfoWithTimeStamp: ContactInformation = ContactInformation(None, None, None, None, None, None, None, None,
+      emailAddress = Some(EmailAddress(emailAddress)), Some("timestamp"))
+
+    val responseDetailWithContactInfo: ResponseDetail = responseDetail.copy(contactInformation = Some(contactInfo))
+
+    val responseDetailWithTimestamp: ResponseDetail =
+      responseDetail.copy(contactInformation = Some(contactInfoWithTimeStamp))
+
+    val subscriptionResponse: SubscriptionResponse = SubscriptionResponse(
+      SubscriptionDisplayResponse(responseCommon, responseDetail))
+
+    val subscriptionResponseWithContactInfo: SubscriptionResponse = SubscriptionResponse(
+      SubscriptionDisplayResponse(responseCommon, responseDetailWithContactInfo))
+
+    val subscriptionResponseWithTimestamp: SubscriptionResponse = SubscriptionResponse(
+      SubscriptionDisplayResponse(responseCommon, responseDetailWithTimestamp))
+  }
+}
