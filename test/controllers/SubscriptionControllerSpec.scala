819076e06c3c31c358394c69eac54ad37f0b4ab5
vineet <vineet.tyagi@digital.hmrc.gov.uk>
2024-07-22 00:28:45 +0100

    [MMCA-4789] Use of eqTo

--- test/controllers/SubscriptionControllerSpec.scala
+++ test/controllers/SubscriptionControllerSpec.scala
@@ -16,19 +16,18 @@
 
 package controllers
 
-import config.MetaConfig.Platform.{ENROLMENT_IDENTIFIER, ENROLMENT_KEY}
 import models.{EORI, EmailAddress}
-import org.mockito.ArgumentMatchers.eq as is
+import org.mockito.ArgumentMatchers.{any, eq => eqTo}
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.mvc.AnyContentAsEmpty
 import play.api.test.FakeRequest
 import play.api.test.Helpers.*
 import play.api.{Application, inject}
 import services.SubscriptionService
-import uk.gov.hmrc.auth.core.{Enrolment, EnrolmentIdentifier, Enrolments}
 import uk.gov.hmrc.http.NotFoundException
 import utils.SpecBase
-import utils.TestData.EORI_VALUE
+import org.mockito.Mockito.when
+import models.responses.{EmailUnverifiedResponse, EmailVerifiedResponse}
 
 import scala.concurrent.Future
 
@@ -36,9 +35,10 @@ class SubscriptionControllerSpec extends SpecBase {
 
   "SubscriptionController.get" should {
     "return 200 status code" in new Setup {
+
       val subscriptionResponse: EmailVerifiedResponse = EmailVerifiedResponse(None)
 
-      when(mockSubscriptionService.getVerifiedEmail(is(traderEORI)))
+      when(mockSubscriptionService.getVerifiedEmail(eqTo(traderEORI)))
         .thenReturn(Future.successful(subscriptionResponse))
 
       running(app) {
@@ -50,7 +50,7 @@ class SubscriptionControllerSpec extends SpecBase {
     "return 200 status code for unVerified email" in new Setup {
       val subscriptionResponse: EmailUnverifiedResponse = EmailUnverifiedResponse(None)
 
-      when(mockSubscriptionService.getUnverifiedEmail(is(traderEORI)))
+      when(mockSubscriptionService.getUnverifiedEmail(eqTo(traderEORI)))
         .thenReturn(Future.successful(subscriptionResponse))
 
       running(app) {
@@ -62,7 +62,7 @@ class SubscriptionControllerSpec extends SpecBase {
     "return 200 status code for getEmail" in new Setup {
       val subscriptionResponse: EmailVerifiedResponse = EmailVerifiedResponse(Some(EmailAddress("test@email.com")))
 
-      when(mockSubscriptionService.getEmailAddress(is(traderEORI)))
+      when(mockSubscriptionService.getEmailAddress(eqTo(traderEORI)))
         .thenReturn(Future.successful(subscriptionResponse))
 
       running(app) {
@@ -72,7 +72,7 @@ class SubscriptionControllerSpec extends SpecBase {
     }
 
     "return 503 for any error" in new Setup {
-      when(mockSubscriptionService.getVerifiedEmail(is(traderEORI)))
+      when(mockSubscriptionService.getVerifiedEmail(eqTo(traderEORI)))
         .thenReturn(Future.failed(new NotFoundException("ShouldNotReturnThis")))
 
       running(app) {
@@ -82,7 +82,7 @@ class SubscriptionControllerSpec extends SpecBase {
     }
 
     "return 503 for any error in unverified email" in new Setup {
-      when(mockSubscriptionService.getUnverifiedEmail(is(traderEORI)))
+      when(mockSubscriptionService.getUnverifiedEmail(eqTo(traderEORI)))
         .thenReturn(Future.failed(new NotFoundException("ShouldNotReturnThis")))
 
       running(app) {
@@ -92,7 +92,7 @@ class SubscriptionControllerSpec extends SpecBase {
     }
 
     "return 503 for any error in getEmail" in new Setup {
-      when(mockSubscriptionService.getEmailAddress(is(traderEORI)))
+      when(mockSubscriptionService.getEmailAddress(eqTo(traderEORI)))
         .thenReturn(Future.failed(new NotFoundException("ShouldNotReturnThis")))
 
       running(app) {
@@ -104,12 +104,7 @@ class SubscriptionControllerSpec extends SpecBase {
 
   trait Setup {
 
-    val traderEORI: EORI = EORI(EORI_VALUE)
-    val enrolments: Enrolments =
-      Enrolments(
-        Set(Enrolment(ENROLMENT_KEY,
-          Seq(EnrolmentIdentifier(ENROLMENT_IDENTIFIER, traderEORI.value)),
-          "activated")))
+    val traderEORI: EORI = EORI("test_eori")
 
     val request: FakeRequest[AnyContentAsEmpty.type] =
       FakeRequest("GET", controllers.routes.SubscriptionController.getVerifiedEmail().url)
@@ -120,13 +115,9 @@ class SubscriptionControllerSpec extends SpecBase {
     val unVerifiedEmailRequest: FakeRequest[AnyContentAsEmpty.type] =
       FakeRequest("GET", controllers.routes.SubscriptionController.getUnverifiedEmail().url)
 
-    val mockAuthConnector: CustomAuthConnector = mock[CustomAuthConnector]
     val mockSubscriptionService: SubscriptionService = mock[SubscriptionService]
 
-    when(mockAuthConnector.authorise[Enrolments](any, any)(any, any)).thenReturn(Future.successful(enrolments))
-
     val app: Application = GuiceApplicationBuilder().overrides(
-      inject.bind[CustomAuthConnector].toInstance(mockAuthConnector),
       inject.bind[SubscriptionService].toInstance(mockSubscriptionService)
     ).configure(
       "microservice.metrics.enabled" -> false,
